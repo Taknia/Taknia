cc84ad9c51c887e9056d34126b065778848074c6
Taknia Soft <69820886+Taknia@users.noreply.github.com>
2025-02-20 02:33:52 +0200

    update addons

--- sale_report/models/models.py
+++ sale_report/models/models.py
@@ -1,10 +1,7 @@
 from odoo import models, fields
 
-class SaleReportCustom(models.Model):
-    _name = 'sale.report.custom'  # تحقق من هذا الاسم
-    _description = 'Custom Sale Report'
-
-    name = fields.Char(string="Report Name")
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'  # توريث من النموذج الأصلي
 
     car_type = fields.Char(string="Car Type")
     car_license_plate_no = fields.Char(string="License Plate Number")
