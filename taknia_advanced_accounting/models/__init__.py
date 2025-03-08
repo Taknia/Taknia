9db64d971346a0b8f69d042838900a192e0f656c
Taknia Soft <69820886+Taknia@users.noreply.github.com>
2025-03-08 03:02:04 +0200

    update addons

--- taknia_advanced_accounting/models/__init__.py
+++ taknia_advanced_accounting/models/__init__.py
@@ -1,4 +1,4 @@
-
+# استيراد الملفات الرئيسية الأخرى
 from . import accounting_report
 from . import branch_comparison_report
 from . import financial_kpi
@@ -16,5 +16,6 @@ from . import branch
 from . import branch_performance
 from . import branch_budget
 
+# استيراد kpi_dashboard داخل دالة متأخرة لتجنب التداخل
 def load_kpi_dashboard():
-    from . import kpi_dashboard
+    from . import kpi_dashboard  # التأكد من أن الاستيراد فقط عند الحاجة
